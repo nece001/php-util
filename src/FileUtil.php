86fdc6b3e65df28016c17bb16966edc72da652b2
nece001 <nece001@163.com>
2023-06-27 20:58:23 +0800

    更加文件助手方法

--- src/FileUtil.php
+++ src/FileUtil.php
@@ -11,6 +11,336 @@ namespace Nece\Util;
 class FileUtil
 {
     /**
+     * 读取文件内容
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $filename
+     *
+     * @return string
+     */
+    public static function read($filename)
+    {
+        $content = '';
+        if (function_exists('file_get_contents')) {
+            @$content = file_get_contents($filename);
+        } else {
+            if (@$fp = fopen($filename, 'r')) {
+                @$content = fread($fp, filesize($filename));
+                @fclose($fp);
+            }
+        }
+        return $content;
+    }
+
+    /**
+     * 写入文件
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $filename
+     * @param string $writetext
+     * @param string $openmod
+     *
+     * @return bool
+     */
+    public static function write($filename, $writetext, $openmod = 'w')
+    {
+        if (@$fp = fopen($filename, $openmod)) {
+            flock($fp, 2);
+            fwrite($fp, $writetext);
+            fclose($fp);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 删除文件或清空目录（不删除目录本身）
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $dirname
+     *
+     * @return boolean
+     */
+    public static function clear($dirname)
+    {
+        if (!file_exists($dirname)) {
+            return false;
+        }
+
+        if (is_file($dirname) || is_link($dirname)) {
+            return unlink($dirname);
+        }
+
+        $dir = dir($dirname);
+        while (false !== $entry = $dir->read()) {
+            if ($entry == '.' || $entry == '..') {
+                continue;
+            }
+            self::clear($dirname . DIRECTORY_SEPARATOR . $entry);
+        }
+        $dir->close();
+    }
+
+    /**
+     * 删除文件或目录（目录也删除）
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-17
+     *
+     * @param string $dirname
+     *
+     * @return boolean
+     */
+    public static function rm($dirname)
+    {
+        self::clear($dirname);
+        return rmdir($dirname);
+    }
+
+    /**
+     * 删除文件或目录（目录也删除）
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $dirname
+     *
+     * @return boolean
+     */
+    public static function delete($dirname)
+    {
+        return self::rm($dirname);
+    }
+
+    /**
+     * 复制
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $src 源文件或目录的路径
+     * @param string $dist 目标路径
+     *
+     * @return boolean
+     */
+    public static function copy($src, $dist)
+    {
+        if (!file_exists($src)) {
+            return false;
+        }
+
+        if (is_file($src)) {
+            return copy($src, $dist);
+        }
+
+        $src = self::fixFilePath($src);
+        $dist = self::fixFilePath($dist);
+        if (!file_exists($dist)) {
+            self::mkdir($dist);
+        }
+
+        $file = opendir($src);
+        while ($fileName = readdir($file)) {
+            $file1 = $src . DIRECTORY_SEPARATOR . $fileName;
+            $file2 = $dist . DIRECTORY_SEPARATOR . $fileName;
+
+            if ($fileName != '.' && $fileName != '..') {
+                if (is_dir($file1)) {
+                    self::copy($file1, $file2);
+                } else {
+                    copy($file1, $file2);
+                }
+            }
+        }
+        closedir($file);
+        return true;
+    }
+
+    /**
+     * 创建文件夹
+     *
+     * @param $dir
+     * @return bool
+     */
+    public static function mkdir($dir)
+    {
+        $dir = self::fixFilePath($dir);
+        if (!is_dir($dir)) {
+            return mkdir($dir, 0700, true);
+        }
+        return true;
+    }
+
+    /**
+     * 遍历获取目录下的指定类型的文件
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $path
+     * @param string $preg
+     *
+     * @return array
+     */
+    public static function listFile($path, $preg = "/\.(gif|jpeg|jpg|png|bmp)$/i")
+    {
+        $files = array();
+
+        if (!is_dir($path)) {
+            return $files;
+        }
+
+        $handle = opendir($path);
+        while (false !== ($file = readdir($handle))) {
+            if ($file != '.' && $file != '..') {
+                $path2 = $path . DIRECTORY_SEPARATOR . $file;
+
+                if (is_dir($path2)) {
+                    $tmp = self::listFile($path2, $files);
+                    $files = array_merge($files, $tmp);
+                } else {
+                    if ($preg) {
+                        if (preg_match($preg, $file)) {
+                            $files[] = $path2;
+                        }
+                    } else {
+                        $files[] = $path2;
+                    }
+                }
+            }
+        }
+        return $files;
+    }
+
+    /**
+     * 获取指定目录下的子目录和文件
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $dir
+     *
+     * @return array
+     */
+    public static function listDir($dir)
+    {
+        $dir = self::fixFilePath($dir);
+        $dirArray = [];
+        if (false != ($handle = opendir($dir))) {
+            while (false !== ($file = readdir($handle))) {
+                if (!in_array($file, array('.', '..'))) {
+                    if (is_dir($dir . $file)) { //判断是否文件夹
+                        $dirArray['dir'][] = $file;
+                    } else {
+                        $dirArray['file'][] = $file;
+                    }
+                }
+            }
+            closedir($handle);
+        }
+        return $dirArray;
+    }
+
+    /**
+     * 计算目录空间大小（字节数）
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $dir
+     *
+     * @return int
+     */
+    public static function dirSize($dir)
+    {
+        if (!self::readable($dir)) {
+            return 0;
+        }
+
+        $dir_list = opendir($dir);
+        $dir_size = 0;
+        while (false !== ($folder_or_file = readdir($dir_list))) {
+            if ($folder_or_file != "." && $folder_or_file != "..") {
+                if (is_dir("$dir/$folder_or_file")) {
+                    $dir_size += self::dirSize("$dir/$folder_or_file");
+                } else {
+                    $dir_size += filesize("$dir/$folder_or_file");
+                }
+            }
+        }
+        closedir($dir_list);
+        return $dir_size;
+    }
+
+    /**
+     * 是否可读
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $dir
+     *
+     * @return boolean
+     */
+    public static function readable($fiename)
+    {
+        return is_readable($fiename);
+    }
+
+    /**
+     * 是否可写
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $dir
+     *
+     * @return boolean
+     */
+    public static function writeable($dir)
+    {
+        if (is_dir($dir)) {
+            if (is_writable($dir)) {
+                $objects = scandir($dir);
+                foreach ($objects as $object) {
+                    if ($object != "." && $object != "..") {
+                        if (!self::writeable($dir . DIRECTORY_SEPARATOR . $object)) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            } else {
+                return false;
+            }
+        } else if (file_exists($dir)) {
+            return (is_writable($dir));
+        }
+    }
+
+    /**
+     * 检测是否为空文件夹
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $dir
+     *
+     * @return boolean
+     */
+    public static function emptyDir($dir)
+    {
+        return (($files = @scandir($dir)) && count($files) <= 2);
+    }
+
+    /**
      * 计算字节数,默认单位为M
      * 
      * @Author nece001@163.com
@@ -169,6 +499,26 @@ class FileUtil
     }
 
     /**
+     * 修正文件路径
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-27
+     *
+     * @param string $path 路径
+     * @param boolean $is_dir 是否文件
+     *
+     * @return string
+     */
+    public static function fixFilePath($path, $is_file = false)
+    {
+        $path = rtrim(str_replace('/', DIRECTORY_SEPARATOR, $path), DIRECTORY_SEPARATOR);
+        if (!$is_file) {
+            $path .= DIRECTORY_SEPARATOR;
+        }
+        return $path;
+    }
+
+    /**
      * 修正路径,去除两端的/,如果是路径补齐最后一个/
      * 
      * @Author nece001@163.com
@@ -179,7 +529,7 @@ class FileUtil
      * 
      * @return string
      */
-    public static function fixPath($path, $is_file = false)
+    public static function fixUriPath($path, $is_file = false)
     {
         $path = trim(preg_replace('/\/{2,}/', '/', str_replace('\\', '/', $path)), '/');
         if (!$is_file) {
@@ -199,7 +549,7 @@ class FileUtil
      * 
      * @return string
      */
-    public static function fixUrl($current, $target)
+    public static function buildAbsoluteUrl($current, $target)
     {
         $target = str_replace('\\', '/', $target);
         if (false === strpos($target, '://')) {
